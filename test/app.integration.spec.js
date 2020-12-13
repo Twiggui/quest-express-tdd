7e37afb5f1b745f7a2237bcf81ca5b699774cc00
Guillem <guillem.dardill@gmail.com>
2020-12-13 15:56:48 +0100

    challenge done

--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,7 +1,9 @@
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 describe('Test routes', () => {
+  beforeEach((done) => connection.query('TRUNCATE bookmark', done));
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -12,5 +14,69 @@ describe('Test routes', () => {
         expect(response.body).toEqual(expected);
         done();
       });
+  });
+  it('POST /bookmarks send empty object return 422 code', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const expected = { error: 'required field(s) missing' };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+  it('POST /bookmarks send object send back object', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const expected = {
+          id: 1,
+          url: 'https://jestjs.io',
+          title: 'Jest',
+        };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
+  describe('GET /bookmarks/:id', () => {
+    const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+    beforeEach((done) =>
+      connection.query('TRUNCATE bookmark', () =>
+        connection.query('INSERT INTO bookmark SET ?', testBookmark, done)
+      )
+    );
+
+    it("GET /bookmark/:id id doesn't exist send back error", (done) => {
+      request(app)
+        .get('/bookmarks/2')
+        .expect(404)
+        .expect('Content-Type', /json/)
+        .then((response) => {
+          const expected = { error: 'Bookmark not found' };
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
+    it('GET /bookmark/:id send back an object as json', (done) => {
+      request(app)
+        .get('/bookmarks/1')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then((response) => {
+          const expected = {
+            id: 1,
+            url: 'https://nodejs.org/',
+            title: 'Node.js',
+          };
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
   });
 });
